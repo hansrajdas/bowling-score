51189a46f9c1e437b44a7f6e9fba940f215ee9ac
Hansraj Das <raj.das.136@gmail.com>
2018-08-22 22:41:25 +0530

    put handler created

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+  def get(self):
+    return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+  def put(self, pins_knocked):
+    return pins_knocked + '-test'
+
+api.add_resource(HelloWorld,
+                 '/',
+                 '/get-score',
+                 '/pins-knocked/<int:pins_knocked>')
 
 if __name__ == '__main__':
     app.run(debug=True)
