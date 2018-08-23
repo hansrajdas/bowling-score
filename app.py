ac786a7e7069d8cfeaa868af2926457802fbd9d9
Hansraj Das <hansrajd@rodete-desktop-imager.corp.google.com>
2018-08-23 12:34:14 +0530

    All required handler and methods added

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import request
+
+from flask_restful import Api
+from flask_restful import Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class GetScore(Resource):
   def get(self):
-    return {'hello': 'world'}
+    return {1: 'x'}
 
+class StartGame(Resource):
   def post(self):
-    return request.form['data']  + '-test'
+    return "New game started..."
 
-api.add_resource(HelloWorld,
-                 '/',
-                 '/get-score',
-                 '/pins-knocked')
+class PinsKnocked(Resource):
+  def post(self):
+    return "Score updated..."
+
+api.add_resource(StartGame, '/start-game')
+api.add_resource(PinsKnocked, '/pins-knocked')
+api.add_resource(GetScore, '/', '/get-score')
 
 if __name__ == '__main__':
     app.run(debug=True)
