e24d4c702029f8b2ff9a29a311cb6276f8e25a90
Hansraj Das <hansrajd@rodete-desktop-imager.corp.google.com>
2018-08-24 13:36:00 +0530

    pins down for special cases handled

--- game_manager.py
+++ game_manager.py
@@ -14,8 +14,10 @@ class GameManager(object):
 
   def startNewGame(self):
     """Starts a new game."""
+    self.frameCounter = 0
+    self.roll = 0
     self.frameScores = collections.OrderedDict([
-        (frameId, {'tries': {0: 0, 1: 0}, 'score': 0})
+        (frameId, {'tries': {0: -1, 1: -1}, 'score': 0})
         for frameId in range(10)
     ])
     return {'message': 'New game started successfully.'}
@@ -28,9 +30,12 @@ class GameManager(object):
                      for frame in self.frameScores)
     return {'frame-scores': self.frameScores, 'total-score': totalScore}
 
-  @staticmethod
-  def _isStrike(pinsKnocked):
-    return pinsKnocked == constants.MAX_PINS
+  def _isStrike(self):
+    return self.frameScores[self.frameCounter]['tries'][0] == constants.MAX_PINS
+
+  def _isSpare(self):
+    tries = self.frameScores[self.frameCounter]['tries']
+    return tries[0] + tries[1] == constants.MAX_PINS
 
   def _validPinsKnocked(self, pinsKnocked):
     if not pinsKnocked.isdigit():
@@ -41,6 +46,14 @@ class GameManager(object):
       return False, 'Pins knocked should be positive number, less than 10.'
 
     frameTries = self.frameScores[self.frameCounter]['tries']
+    if self.frameCounter == constants.TOTAL_FRAMES - 1:
+      if not self._isStrike() and (
+          self.roll == 1 and frameTries[0] + pins > constants.MAX_PINS):
+        return False, constants.INVALID_SECOND_ROLL.format(
+            firstRollPins=frameTries[0])
+      else:
+        return True, ''
+
     if self.roll == 1 and frameTries[0] + pins > constants.MAX_PINS:
       return False, constants.INVALID_SECOND_ROLL.format(
           firstRollPins=frameTries[0])
@@ -49,7 +62,16 @@ class GameManager(object):
 
   def _updateScore(self, pinsKnocked):
     self.frameScores[self.frameCounter]['tries'][self.roll] = pinsKnocked
-    if not self.roll and (not self._isStrike(pinsKnocked)):
+
+    # If strike or spare in last frame.
+    if self.frameCounter == constants.TOTAL_FRAMES - 1:
+      print 'here, frame ctr is 9 and roll', self.roll
+      if (not self.roll) or (
+          self.roll < 2 and (self._isStrike() or self._isSpare())):
+        self.roll += 1
+      else:
+        self.frameCounter += 1
+    elif not self._isStrike():
       self.roll += 1
     else:
       self.roll = 0
